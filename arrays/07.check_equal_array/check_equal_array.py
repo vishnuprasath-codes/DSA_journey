c4a7247f18a7a5e811fec20c66f906b930bc3a4e
vishnuprasath-codes <vishnu1sde@gmail.com>
2025-07-09 21:24:30 +0530

    Update check_equal_array.py

--- arrays/07.check_equal_array/check_equal_array.py
+++ arrays/07.check_equal_array/check_equal_array.py
@@ -1,1 +1,7 @@
-
+class Solution:
+    # Function to check if two arrays are equal or not.
+    def checkEqual(self, a, b) -> bool:
+        #code here
+        if len(a) != len(b):
+            return False
+        return sorted(a) == sorted(b)
