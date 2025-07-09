c62ceac83339586c76c898dae91150e7c57f53c3
vishnuprasath-codes <vishnu1sde@gmail.com>
2025-07-09 19:05:10 +0530

    Update missing_and_repeating.py

--- arrays/05.missing_and_repeating/missing_and_repeating.py
+++ arrays/05.missing_and_repeating/missing_and_repeating.py
@@ -1,1 +1,30 @@
+class Solution:
+    def findTwoElement( self,arr): 
+        # code here
+        n = len(arr)  # size of the array
 
+        # Find Sn and S2n:
+        SN = (n * (n + 1)) // 2
+        S2N = (n * (n + 1) * (2 * n + 1)) // 6
+    
+        # Calculate S and S2:
+        S, S2 = 0, 0
+        for i in range(n):
+            S += arr[i]
+            S2 += arr[i] * arr[i]   
+
+        # S-Sn = X-Y:
+        val1 = S - SN
+
+        # S2-S2n = X^2-Y^2:
+        val2 = S2 - S2N
+    
+        # Find X+Y = (X^2-Y^2)/(X-Y):
+        val2 = val2 // val1
+
+        # Find X and Y: X = ((X+Y)+(X-Y))/2 and Y = X-(X-Y),
+        # Here, X-Y = val1 and X+Y = val2:
+        x = (val1 + val2) // 2
+        y = x - val1
+
+        return [x, y]
