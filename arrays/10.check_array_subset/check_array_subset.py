5be065eb828d5456f460230748c195e1c758f34a
vishnuprasath-codes <vishnu1sde@gmail.com>
2025-07-09 21:55:45 +0530

    Update check_array_subset.py

--- arrays/10.check_array_subset/check_array_subset.py
+++ arrays/10.check_array_subset/check_array_subset.py
@@ -1,1 +1,26 @@
+class Solution:
+    #Function to check if a is a subset of b.
+    def isSubset(self, a, b):
+        # Sort both arrays in ascending order
+        a.sort()
+        b.sort()
 
+        i = 0
+        j = 0
+
+        # Traverse both arrays using two pointers
+        while i < len(a) and j < len(b):
+            if a[i] < b[j]:
+                # Element in arr1 is smaller, move to the next element in a
+                i += 1
+            elif a[i] == b[j]:
+                # Element found in both arrays, move to the next element in both arrays
+                i += 1
+                j += 1
+            else:
+                # Element in arr2 not found in a, not a subset
+                return False
+
+        # If we have traversed all elements in b, it is a subset
+        return j == len(b)
+    
