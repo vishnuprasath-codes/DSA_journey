9e49e64f0d3333b31a9b3e4284efccd7cf10d10c
vishnuprasath-codes <vishnu1sde@gmail.com>
2025-07-09 09:40:38 +0530

    Update min_and_max.py

--- arrays/min&max/min_and_max.py
+++ arrays/min&max/min_and_max.py
@@ -1,10 +1,14 @@
 class Solution:
     def get_min_max(self, arr):
-        max=arr[0]
-        min=arr[0]
+        
+        # Initialize min and max to the first element
+        maximum = arr[0]
+        minimum = arr[0]
+        #linear search for min and max
         for i in arr:
-	        if max<i:
-	            max=i
-            if min>i:
-	            min=i
-        return(min,max)
+            if i > maximum:
+                maximum = i
+            if i < minimum:
+                minimum = i
+        
+        return (minimum, maximum)
