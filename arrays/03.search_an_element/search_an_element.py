8e1bede42ac0241b1798b75bf4c0a3ecf3a73e48
vishnuprasath-codes <vishnu1sde@gmail.com>
2025-07-09 12:42:19 +0530

    Update search_an_element.py

--- arrays/03.search_an_element/search_an_element.py
+++ arrays/03.search_an_element/search_an_element.py
@@ -1,1 +1,8 @@
-
+class Solution:
+    def search(self,arr, x):
+        #linear search using for loop
+        for i in arr:
+            if i==x:
+                return arr.index(i)
+        # return -1 when the search element x is not found
+        return -1
