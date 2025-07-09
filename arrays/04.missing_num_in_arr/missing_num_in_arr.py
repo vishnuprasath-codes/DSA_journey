6b9f526f1a4ea8a753cde1f9c7c84328bac296f3
vishnuprasath-codes <vishnu1sde@gmail.com>
2025-07-09 19:01:37 +0530

    Update missing_num_in_arr.py

--- arrays/04.missing_num_in_arr/missing_num_in_arr.py
+++ arrays/04.missing_num_in_arr/missing_num_in_arr.py
@@ -1,1 +1,7 @@
-
+class Solution:
+    def missingNum(self, arr):
+        
+        n=len(arr)+1 #1 element is missed
+        sumarr=sum(arr) # sum of the array elements
+        exarr=n*(n+1)//2 #sum of natural numbers till the size of the array,included with missed number
+        return exarr-sumarr
