2b1cf3d11a0607a78148e621144f7d350d8e85c1
vishnuprasath-codes <vishnu1sde@gmail.com>
2025-07-09 12:40:17 +0530

    Update third_largest.py

--- arrays/02.third_largest/third_largest.py
+++ arrays/02.third_largest/third_largest.py
@@ -1,1 +1,21 @@
-
+class Solution:
+    def thirdLargest(self,arr):
+        # code here
+        # -1 for non existing number
+        # if the array only contains positive
+        largest=-1
+        slargest=-1
+        tlargest=-1
+        #iterate array
+        for i in arr:
+            if i>largest:
+                tlargest=slargest
+                slargest=largest
+                largest=i
+            elif i>slargest:
+                tlargest=slargest
+                slargest=i
+            elif i>tlargest:
+                tlargest=i
+        # returns the third largest element in the array
+        return tlargest
