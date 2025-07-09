171931e5882de32426f7a505c71d1d42f5fd75e6
vishnuprasath-codes <vishnu1sde@gmail.com>
2025-07-09 21:31:22 +0530

    Update rotate_by_1.py

--- arrays/08.rotate_by_1/rotate_by_1.py
+++ arrays/08.rotate_by_1/rotate_by_1.py
@@ -1,1 +1,11 @@
+class Solution:
+    def rotate(self, arr):
+        # store the last element in a variable
+        lastElement = arr[-1]
 
+        # assign every value by its predecessor
+        for i in range(len(arr) - 1, 0, -1):
+            arr[i] = arr[i - 1]
+
+        # first element will be assigned by last element
+        arr[0] = lastElement
