33b0879c7ab2a510856adaa4e49b2268e57b0a99
vishnuprasath-codes <vishnu1sde@gmail.com>
2025-07-10 21:46:51 +0530

    Update count_freequency.py

--- arrays/11.count_freequency/count_freequency.py
+++ arrays/11.count_freequency/count_freequency.py
@@ -1,1 +1,11 @@
-
+class Solution:
+    def frequencyCount(self, arr):
+        #  code here
+        n=len(arr)
+        freq=[0]*n # array to store count of each elements
+        
+        for i in arr: #iterating the array
+            
+            freq[i-1]=freq[i-1]+1 #store the freq of i at the place of freq[i+1]+1
+                
+        return freq
