c9d5f16d71ce51acf263a2e15826d142769f75a0
vishnuprasath-codes <vishnu1sde@gmail.com>
2025-07-11 18:24:28 +0530

    Update two_sum_2.py

--- arrays/12.two_sum/two_sum_2.py
+++ arrays/12.two_sum/two_sum_2.py
@@ -1,1 +1,18 @@
+class Solution:
+	def twoSum(self, arr, target):
+		# code here
+        seen = {}  # hashmap to store number and its index
+
+        for i in range(len(arr)):
+            complement = target - arr[i]
+
+            if complement in seen:
+                # Found the pair, return their indices
+                return [seen[complement], i]
+
+            # Store the current number with its index
+            seen[arr[i]] = i
+
+        # If no such pair exists
+        return [-1, -1]  # or you can raise an exception
 
