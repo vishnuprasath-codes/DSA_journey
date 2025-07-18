259566edf937edeb3c5e16017167f3bc1c64da09
vishnuprasath-codes <vishnu1sde@gmail.com>
2025-07-18 06:21:24 +0530

    Update three_sum.py

--- arrays/13.three_sum/three_sum.py
+++ arrays/13.three_sum/three_sum.py
@@ -14,3 +14,22 @@ class Solution:
         return False
 
 #OPTIMAL SOLUTION
+class Solution:
+    # Function to find if there exists a triplet in the array arr[] which sums up to target.
+    def hasTripletSum(self, arr, target):
+        # Your Code Here
+        n=len(arr)
+        arr.sort()
+        for i in range (n-2):
+            l=i+1
+            r=n-1
+            
+            required_sum=target-arr[i]
+            while l<r:
+                if arr[l]+arr[r] == required_sum:
+                    return True
+                elif arr[l]+arr[r]< required_sum:
+                    l+=1
+                elif arr[l]+arr[r]>required_sum:
+                    r-=1
+        return False
